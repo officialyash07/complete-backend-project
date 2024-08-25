3c127eeb45272d02ec6b8bb15d7e4210d86feac1
Yash Pathik <yashk71097@gmail.com>
2024-08-25 14:46:39 +0530

    Connect to MongoDB

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// require("dotenv").config({ path: "./env" });
+
+import dotenv from "dotenv";
+
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./env",
+});
+
+connectDB();
+
+// ------------------------------------------------------------------------------------------------//
+
+// First approach to connect to the database.
+
+// import express from "express";
+
+// import mongoose from "mongoose";
+
+// import { DB_NAME } from "./constants";
+
+// const app = express();
+
+// (async () => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+
+//         app.on("error", (error) => {
+//             console.log("Error: ", error);
+//             throw error;
+//         });
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on ${process.env.PORT}`);
+//         });
+//     } catch (error) {
+//         console.log("ERROR: ", error);
+//         throw error;
+//     }
+// })();
+
+// ------------------------------------------------------------------------------------------------//
