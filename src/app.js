4fed2a1138d7eebebefbbf3f21f4eeb03c1e94d3
Yash Pathik <yashk71097@gmail.com>
2024-08-28 13:06:43 +0530

    Complete full setup.

--- src/app.js
+++ src/app.js
@@ -16,7 +16,16 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
-
 app.use(cookieParser());
+
+//Routes
+
+//Router Imports
+
+import userRouter from "./routes/user.routes.js";
+
+//Routes declarations
+
+app.use("/api/v1/users", userRouter);
 
 export { app };
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,8 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const registerUser = asyncHandler(async (req, res) => {
+    // Implement your logic here
+    res.status(200).json({ message: "ok" });
+});
+
+export { registerUser };
